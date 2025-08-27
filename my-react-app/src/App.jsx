b5ed92692013c934c12a68102e8298757aced14b
ekachai.k <ekachai.k@kbtg.tech>
2025-08-27 11:37:31 +0700

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <p>
+          Welcome to your React app built with Vite! 🚀
+        </p>
+        <p>
+          This is a simple Hello World page created following the Vite guide.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
